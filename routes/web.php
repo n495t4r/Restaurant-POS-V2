83fe64a576abf50bb36a59f30cb8c2de51cb3eb9
n495t4r <francis4u1991@yahoo.com>
2024-08-20 17:49:06 +0100

    home routes modified

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/admin');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 
 
