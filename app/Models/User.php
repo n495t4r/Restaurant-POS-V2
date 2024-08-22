01ad74b586da98eece1d0eaeb38ffe9f5baf57a0
n495t4r <francis4u1991@yahoo.com>
2024-08-22 20:24:12 +0100

    worked on EOD report etc

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,16 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
@@ -46,6 +49,12 @@ class User extends Authenticatable
         'password' => 'hashed',
         'is_active' => 'boolean',
     ];
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        // return $panel->getName() === 'peva' && $this->hasVerifiedEmail();
+        return true;
+    }
 
     public function getID()
     {
