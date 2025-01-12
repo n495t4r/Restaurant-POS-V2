1186e94d44e552c8f354479bacc28dec564e2f69
n495t4r <francis4u1991@yahoo.com>
2025-01-12 23:13:58 +0100

    Staff order checkout server error fixed

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Facades\Storage;
 
 
@@ -18,6 +20,7 @@ class Customer extends Model
         'address',
         'avatar',
         'user_id',
+        'created_by',
         'is_active',
     ];
 
@@ -34,7 +37,28 @@ class Customer extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function creator(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
     
+    public static function getTotalOrderAmount($customerId)
+    {
+        $today = Carbon::today();
+
+        $total = OrderItem::whereHas('order', function ($query) use ($customerId, $today) {
+            $query->where('customer_id', $customerId)
+                ->where('channel_id', 6)
+                ->whereDate('created_at', $today)
+                ->where('status', '!=', 0);
+        })->sum('price');
+
+        // If no results found, $total will be null, so we coalesce it to 0
+        $total = floatval($total);
+
+        return $total;
+    }
     
     public function getCustomerName()
     {
